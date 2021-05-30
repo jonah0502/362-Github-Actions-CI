3e9398c6df8a2720b93f29a9427b7436027a397d
jonah0502 <jonah0502@gmail.com>
2021-05-29 18:01:22 -0700

    added mult and div functionality and tests

--- test.py
+++ test.py
@@ -10,6 +10,12 @@ class TestCase(unittest.TestCase):
     def test_subtract_1(self):
         self.assertEqual(example.subtract(1, 1), 0)
 
+    def test_multiply_1(self):
+        self.assertEqual(example.multiply(2, 3), 6)
+
+    def test_divide_1(self):
+        self.assertEqual(example.divide(9, 3), 3)
+
 
 if __name__ == "__main__":
     unittest.main()
